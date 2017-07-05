fef28be2cb1e9479d0f616a2481ac3eccd92d233
rgbrobot <robertgarvin@gmail.com>
2017-07-05 14:57:59 -0400

    Fixed the navbar. Previously, it would not expand on smaller screens. now it seems to work fine.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 
+//= require jquery
+ //= require jquery_ujs
 //= require turbolinks
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
-
-
