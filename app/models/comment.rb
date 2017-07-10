d48484b1d3ff4af1551391d15a3026583d512ffd
rgbrobot <robertgarvin@gmail.com>
2017-07-10 18:06:35 -0400

    Added dropdown functionality for ratings. added 5-star system of ratings.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+
+  RATINGS = {
+    '⭑': '1_star',
+    '⭑⭑': '2_stars',
+    '⭑⭑⭑': '3_stars',
+    '⭑⭑⭑⭑': '4_stars',
+    '⭑⭑⭑⭑⭑': '5_stars'
+  }
+
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
