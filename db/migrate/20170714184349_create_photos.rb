eeacb38d821d7c2ca136aeef707db1e50e3bf84c
rgbrobot <robertgarvin@gmail.com>
2017-07-17 12:41:17 -0400

    Uploaded code for review. Also super sleepy.

--- db/migrate/20170714184349_create_photos.rb
+++ db/migrate/20170714184349_create_photos.rb
@@ -6,7 +6,6 @@ class CreatePhotos < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
-    add_index :photos, [:place_id]
-    add_index :photos, :place_id
+      add_index :photos, :place_id
   end
 end
